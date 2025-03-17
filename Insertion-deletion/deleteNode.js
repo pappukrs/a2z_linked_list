a85d8f63e9f2e46fabd2b62311a556932e55805e
pappukrs <digitalpappu101@gmail.com>
2025-03-17 10:51:08 +0530

    delete node

--- Insertion-deletion/deleteNode.js
+++ Insertion-deletion/deleteNode.js
@@ -1,6 +1,53 @@
+
+class ListNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+
 function deleteNode(node) {
     if (!node || !node.next) return; // Cannot delete if it's last node
 
     node.value = node.next.value; // Copy next node's value
     node.next = node.next.next;   // Remove next node
 }
+
+
+// Test case 1: Delete the head node
+let head = new ListNode(10);
+deleteNode(head);
+console.log(head.value); // Expected output: 10
+console.log(head.next);  // Expected output: null
+
+
+
+// Test case 2: Delete a node in the middle of the list
+let head2 = new ListNode(10);
+head2.next = new ListNode(20);
+head2.next.next = new ListNode(30);
+deleteNode(head2.next);
+console.log(head2.value); // Expected output: 10
+console.log(head2.next.value); // Expected output: 30
+console.log(head2.next.next);  // Expected output: null
+
+
+
+// Test case 3: Delete the last node
+let head3 = new ListNode(10);
+head3.next = new ListNode(20);
+head3.next.next = new ListNode(30);
+deleteNode(head3.next.next);
+console.log(head3.value); // Expected output: 10
+console.log(head3.next.value); // Expected output: 20
+console.log(head3.next.next);  // Expected output: null
+
+
+
+
+
+    
+
+
+
--- Insertion-deletion/deleteNthFromEnd.js
+++ Insertion-deletion/deleteNthFromEnd.js
@@ -1,3 +1,10 @@
+class ListNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
 function deleteNthFromEnd(head, n) {
     let dummy = new ListNode(0);
     dummy.next = head;
@@ -15,3 +22,64 @@ function deleteNthFromEnd(head, n) {
     slow.next = slow.next.next; // Remove the nth node
     return dummy.next;
 }
+
+
+// Test case 1: Delete the head node
+let head = new ListNode(10);
+deleteNthFromEnd(head, 1);
+console.log(head.value); // Expected output: 10
+console.log(head.next);  // Expected output: null
+
+
+// Test case 2: Delete a node in the middle of the list
+let head2 = new ListNode(10);
+head2.next = new ListNode(20);
+head2.next.next = new ListNode(30);
+deleteNthFromEnd(head2, 2);
+console.log(head2.value); // Expected output: 10
+console.log(head2.next.value); // Expected output: 30
+console.log(head2.next.next);  // Expected output: null
+
+
+// Test case 3: Delete the last node
+let head3 = new ListNode(10);   
+head3.next = new ListNode(20);
+head3.next.next = new ListNode(30);
+deleteNthFromEnd(head3, 1);
+console.log(head3.value); // Expected output: 10
+console.log(head3.next.value); // Expected output: 20
+console.log(head3.next.next);  // Expected output: null
+
+
+// Test case 4: Delete the second node from the end
+let head4 = new ListNode(10);
+head4.next = new ListNode(20);
+head4.next.next = new ListNode(30);
+deleteNthFromEnd(head4, 2);
+console.log(head4.value); // Expected output: 10
+console.log(head4.next.value); // Expected output: 30
+console.log(head4.next.next);  // Expected output: null
+
+
+// Test case 5: Delete the third node from the end
+let head5 = new ListNode(10);
+head5.next = new ListNode(20);
+head5.next.next = new ListNode(30);
+deleteNthFromEnd(head5, 3);
+console.log(head5.value); // Expected output: 20
+console.log(head5.next.value); // Expected output: 30
+console.log(head5.next.next);  // Expected output: null
+
+
+
+
+
+
+
+
+
+
+
+
+
+
